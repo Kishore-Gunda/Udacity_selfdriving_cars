439fb141132cd8b7fe91750b38e06f9f573bcfe2
Kishore-Gunda <kishore.gunda01@gmail.com>
2018-09-09 17:16:35 +0200

    final

--- Term2/PID_Control/CarND-PID-Control-Project-master/src/PID.h
+++ Term2/PID_Control/CarND-PID-Control-Project-master/src/PID.h
@@ -12,31 +12,14 @@ public:
 
   /*
   * Coefficients
-  */
+  */ 
   double Kp;
   double Ki;
   double Kd;
-  //Twiddle variables
-  double params[3];
-  bool twiddle;
-  double d_params[3];
-  unsigned int loop_counter=0;
-  unsigned int state_counter =0;
-  double twd_error;
-  bool initial_settle= false;
-  double best_error =0;
-  bool measurement=false;
-  bool bad_measurement = false;
-  unsigned int loop_count_max=100;
 
-  /*
-  * OLD CTE Errors
-  */
   double prev_cte;
   bool error_initial;
-
-  //double p_tht=0.2;
-
+  
   /*
   * Constructor
   */
@@ -49,8 +32,8 @@ public:
 
   /*
   * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd, bool twiddle);
+  */ 
+  void Init(double Kp, double Ki, double Kd);
 
   /*
   * Update the PID error variables given cross track error.
