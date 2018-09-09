b15701d35a405e7437118ac25df5ae6370ef9e06
Kishore-Gunda <kishore.gunda01@gmail.com>
2018-09-09 14:37:48 +0200

    Add PID control project
    Add PID control project

---
+++ Term2/PID_Control/CarND-PID-Control-Project-master/src/PID.cpp
@@ -0,0 +1,33 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+}
+
+void PID::UpdateError(double cte) {
+  if(error_initial == false){
+    error_initial = true;
+    prev_cte = cte;
+}
+  this->p_error = cte;
+  this->d_error =  cte - this->prev_cte;
+  this->i_error += cte;
+}
+}
+
+double PID::TotalError() {
+  return (this->Kp*this->p_error) + (this->Ki*this->i_error) +(this->Kd*this->d_error)
+}
+
